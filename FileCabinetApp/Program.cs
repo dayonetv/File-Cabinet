69de8036327e452c4ceecda9e79c9cb393d27ce3
dayonetv <breaking700@gmail.com>
2021-12-18 13:33:41 +0300

    Refactor user input validation.

--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -24,6 +24,18 @@ namespace FileCabinetApp
         private const int AmountOfInputArgsForFullMode = 1;
         private const char FullStartupModeSeparator = '=';
 
+        private const int DefaultMaxNameLength = 60;
+        private const int CustomMaxNameLength = 100;
+        private const int DefaultMinSalary = 0;
+        private const int CustomMinSalary = 300;
+        private const short DefaultMaxHeight = 220;
+        private const short CustomMaxHeight = 250;
+
+        private static readonly Predicate<char> DefaultGenderPredicate = new ((sex) => char.IsLetter(sex));
+        private static readonly Predicate<char> CustomGenderPredicate = new ((sex) => !char.IsWhiteSpace(sex));
+        private static readonly Predicate<DateTime> DefaultDateOfBirthPredicate = new ((date) => date < DateTime.Now);
+        private static readonly Predicate<DateTime> CustomDateOfBirthPredicate = new ((date) => date <= DateTime.Now);
+
         private static readonly Tuple<string, Action<string>>[] Commands = new Tuple<string, Action<string>>[]
         {
             new Tuple<string, Action<string>>("help", PrintHelp),
@@ -67,6 +79,7 @@ namespace FileCabinetApp
 
         private static bool isRunning = true;
         private static IFileCabinetService fileCabinetService;
+        private static IRecordValidator chosenValidator;
 
         /// <summary>
         /// The main console-application entry point.
@@ -79,7 +92,7 @@ namespace FileCabinetApp
                 throw new ArgumentNullException(nameof(args));
             }
 
-            IRecordValidator chosenValidator = ValidateInputArgs(args);
+            chosenValidator = ValidateInputArgs(args);
 
             Console.WriteLine($"File Cabinet Application, developed by {Program.DeveloperName}");
 
@@ -210,15 +223,10 @@ namespace FileCabinetApp
 
                     isValid = true;
                 }
-                catch (FormatException ex)
-                {
-                    isValid = false;
-                    Console.WriteLine(ex.Message);
-                }
                 catch (ArgumentException ex)
                 {
                     isValid = false;
-                    Console.WriteLine($"\n{ex.Message}.\nPlease try again. ");
+                    Console.WriteLine($"\n{ex.Message}. Please try again. ");
                 }
             }
             while (!isValid);
@@ -229,22 +237,22 @@ namespace FileCabinetApp
             CreateEditParameters parameters = new ();
 
             Console.Write("First Name: ");
-            parameters.FirstName = Console.ReadLine().Trim();
+            parameters.FirstName = ReadInput(StringConverter, FirstNameValidator);
 
             Console.Write("Last Name: ");
-            parameters.LastName = Console.ReadLine().Trim();
+            parameters.LastName = ReadInput(StringConverter, LastNameValidator);
 
             Console.Write("Date Of Birth: ");
-            parameters.DateOfBirth = DateTime.ParseExact(Console.ReadLine().Trim(), "d", CultureInfo.InvariantCulture);
+            parameters.DateOfBirth = ReadInput(DateConverter, DateOfBirthValidator);
 
             Console.Write("Height: ");
-            parameters.Height = short.Parse(Console.ReadLine().Trim(), CultureInfo.InvariantCulture);
+            parameters.Height = ReadInput(ShortConverter, HeightValidator);
 
             Console.Write("Salary: ");
-            parameters.Salary = decimal.Parse(Console.ReadLine().Trim(), CultureInfo.InvariantCulture);
+            parameters.Salary = ReadInput(DecimalConverter, SalaryValidator);
 
             Console.Write("Sex: ");
-            parameters.Sex = Console.ReadKey().KeyChar;
+            parameters.Sex = ReadInput(CharConverter, GenderValidator);
 
             return parameters;
         }
@@ -283,15 +291,10 @@ namespace FileCabinetApp
                         isValid = true;
                     }
                 }
-                catch (FormatException ex)
-                {
-                    isValid = false;
-                    Console.WriteLine($"\n{ex.Message}. Please try again.");
-                }
                 catch (ArgumentException ex)
                 {
                     isValid = false;
-                    Console.WriteLine($"\n{ex.Message}.\nPlease try again.");
+                    Console.WriteLine($"\n{ex.Message}. Please try again.");
                 }
             }
             while (!isValid);
@@ -347,6 +350,107 @@ namespace FileCabinetApp
         private static ReadOnlyCollection<FileCabinetRecord> FindByLastName(string lastName)
         {
             return fileCabinetService.FindByLastName(lastName.Trim('"'));
+        }
+
+        private static T ReadInput<T>(Func<string, Tuple<bool, string, T>> converter, Func<T, Tuple<bool, string>> validator)
+        {
+            do
+            {
+                T value;
+
+                var input = Console.ReadLine();
+                var conversionResult = converter(input);
+
+                if (!conversionResult.Item1)
+                {
+                    Console.WriteLine($"Conversion failed: {conversionResult.Item2}. Please, correct your input.");
+                    continue;
+                }
+
+                value = conversionResult.Item3;
+
+                var validationResult = validator(value);
+                if (!validationResult.Item1)
+                {
+                    Console.WriteLine($"Validation failed: {validationResult.Item2}. Please, correct your input.");
+                    continue;
+                }
+
+                return value;
+            }
+            while (true);
+        }
+
+        private static Tuple<bool, string, string> StringConverter(string input)
+        {
+            input = input.Trim();
+            bool result = !string.IsNullOrEmpty(input);
+            return new Tuple<bool, string, string>(result, input, input);
+        }
+
+        private static Tuple<bool, string, DateTime> DateConverter(string input)
+        {
+            input = input.Trim();
+            bool result = DateTime.TryParseExact(input, "d", CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out DateTime date);
+            return new Tuple<bool, string, DateTime>(result, input, date);
+        }
+
+        private static Tuple<bool, string, char> CharConverter(string input)
+        {
+            input = input.Trim();
+            bool result = char.TryParse(input, out char symbol);
+            return new Tuple<bool, string, char>(result, input, symbol);
+        }
+
+        private static Tuple<bool, string, decimal> DecimalConverter(string input)
+        {
+            input = input.Trim();
+            bool result = decimal.TryParse(input, out decimal decValue);
+            return new Tuple<bool, string, decimal>(result, input, decValue);
+        }
+
+        private static Tuple<bool, string, short> ShortConverter(string input)
+        {
+            input = input.Trim();
+            bool result = short.TryParse(input, out short intValue);
+            return new Tuple<bool, string, short>(result, input, intValue);
+        }
+
+        private static Tuple<bool, string> FirstNameValidator(string firstName)
+        {
+            bool result = firstName.Length < (chosenValidator is DefaultValidator ? DefaultMaxNameLength : CustomMaxNameLength);
+            return new Tuple<bool, string>(result, result ? "Valid" : "first name is too long");
+        }
+
+        private static Tuple<bool, string> LastNameValidator(string lastName)
+        {
+            bool result = lastName.Length < (chosenValidator is DefaultValidator ? DefaultMaxNameLength : CustomMaxNameLength);
+            return new Tuple<bool, string>(result, result ? "Valid" : "last name is too long");
+        }
+
+        private static Tuple<bool, string> DateOfBirthValidator(DateTime dateOfBirth)
+        {
+            bool result = chosenValidator is DefaultValidator ? DefaultDateOfBirthPredicate(dateOfBirth) : CustomDateOfBirthPredicate(dateOfBirth);
+            return new Tuple<bool, string>(result, result ? "Valid" : "wrong date of birth");
+        }
+
+        private static Tuple<bool, string> HeightValidator(short height)
+        {
+            bool result = height < (chosenValidator is DefaultValidator ? DefaultMaxHeight : CustomMaxHeight);
+            return new Tuple<bool, string>(result, result ? "Valid" : "height is too big");
+        }
+
+        private static Tuple<bool, string> SalaryValidator(decimal salary)
+        {
+            int minSalary = chosenValidator is DefaultValidator ? DefaultMinSalary : CustomMinSalary;
+            bool result = salary >= minSalary;
+            return new Tuple<bool, string>(result, result ? "Valid" : $"salary can not be less than {minSalary}");
+        }
+
+        private static Tuple<bool, string> GenderValidator(char sex)
+        {
+            bool result = chosenValidator is DefaultValidator ? DefaultGenderPredicate(sex) : CustomGenderPredicate(sex);
+            return new Tuple<bool, string>(result, result ? "Valid" : "gender wrong format");
         }
     }
 }
