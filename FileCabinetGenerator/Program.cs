ed6d61f02d50d47ef2e5eb681021c52cc30cf6b4
dayonetv <breaking700@gmail.com>
2021-12-24 19:18:58 +0300

    Add FileCabinetGenerator console application to the solution

---
+++ FileCabinetGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FileCabinetGenerator
+{
+    /// <summary>
+    /// Generator of file cabinet reacords.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main console-application entry point.
+        /// </summary>
+        /// <param name="args">Applicattion startup parameters. </param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
