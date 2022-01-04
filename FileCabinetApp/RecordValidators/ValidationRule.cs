a513e67cee035994fdc944e20ec912d318463ed2
dayonetv <breaking700@gmail.com>
2022-01-04 21:13:22 +0300

    Add loading validation rules from json file.

---
+++ FileCabinetApp/RecordValidators/ValidationRule.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCabinetApp
+{
+    /// <summary>
+    /// Validation rule properties for json file.
+    /// </summary>
+    public class ValidationRule
+    {
+        /// <summary>
+        /// Gets valid firstname length range.
+        /// </summary>
+        /// <value></value>
+        public Range FirstName { get; init; }
+
+        /// <summary>
+        /// Gets valid lastname length range.
+        /// </summary>
+        /// <value></value>
+        public Range LastName { get; init; }
+
+        /// <summary>
+        /// Gets valid date of birth range.
+        /// </summary>
+        /// <value></value>
+        public DateRange DateOfBirth { get; init; }
+
+        /// <summary>
+        /// Gets valid salary range.
+        /// </summary>
+        /// <value></value>
+        public Range Salary { get; init; }
+
+        /// <summary>
+        /// Gets valid height range.
+        /// </summary>
+        /// <value></value>
+        public Range Height { get; init; }
+
+        /// <summary>
+        /// Gets valid gerders.
+        /// </summary>
+        /// <value></value>
+        public List<char> Genders { get; init; }
+    }
+}
